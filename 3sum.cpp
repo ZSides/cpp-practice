624b237c8584212dd0e86b7d39d29d78b734c8e2
Ivan <ivanlebeev.90@gmail.com>
2020-03-03 00:37:22 +0300

    https://en.wikipedia.org/wiki/Subset_sum_problem

--- 3sum.cpp
+++ 3sum.cpp
@@ -7,15 +7,13 @@ using namespace std;
 int main() {
     vector<int> a = {1, 3, 5, 4, 2, 5, 7, -1, -2, -3};
     vector<int> b = {2, 4, 5, 6, 1, -1};
-    unordered_set<int> c = {1, 2, 3, 4, -1, -5};
-    unordered_set<int>::iterator cfind, cend = c.end();
+    unordered_multiset<int> c = {1, 2, -5, -5, 3, 4, -1, -5};
+    int answer = 0;
     for (auto ai = a.begin(), aie = a.end(); ai != aie; ++ai) {
         for (auto bi = b.begin(), bie = b.end(); bi != bie; ++bi) {
-            cfind = c.find(-*ai - *bi);
-            if (cfind != cend) {
-                cout << *ai << " + " << *bi << " + " << *cfind << " = 0; ";
-            }
+            answer += c.count(-*ai - *bi);
         }
     }
+    cout << answer << '\n';
     return 0;
 }
